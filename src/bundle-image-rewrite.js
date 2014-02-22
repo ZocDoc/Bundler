d3fef3c23cda28f4a0c91750859db046763cadea
Andrew-Hagedorn-ZocDoc <andrew.hagedorn@zocdoc.com>
2014-02-22 11:36:50 -0500

    add class for image re-writing and tests

---
+++ src/bundle-image-rewrite.js
@@ -0,0 +1,77 @@
+/*
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+var ext = require('./string-extensions.js'),
+    fs = require("fs"),
+    hasher = require('crypto');
+
+function BundleImageRewriter(
+    fileSystem,
+    outputRoot,
+    rootPath
+) {
+    fileSystem = fileSystem || fs;
+
+    if(!outputRoot.endsWith('/')) {
+        outputRoot = outputRoot + '/';
+    }
+
+    if (!rootPath.endsWith('/')) {
+        rootPath = rootPath + '/';
+    }
+
+
+    var generateHashOfFile = function (filepath)
+    {
+        var fileText = fileSystem.readFileSync(filepath);
+        return hasher.createHash('md5').update(fileText).digest('hex');
+    };
+
+    this.rewriteUrl = function (url) {
+         
+        var cleanedUrl = url.replace("url(", "").replace(")", "").replace(/'/g, "").replace(/"/g, "");
+        var filepath = rootPath + cleanedUrl;
+        var exists = fileSystem.existsSync(filepath);
+        if (!exists) {
+            return cleanedUrl;
+        }
+
+        var hash = generateHashOfFile(filepath);
+        
+        return outputRoot + 'version-' + hash + '/' + cleanedUrl;
+    }
+
+    this.imageUrlRegex = new RegExp(/url\(.*?g['"]?\)/ig)
+}
+
+exports.BundleImageRewriter = BundleImageRewriter;
+
+BundleImageRewriter.prototype.VersionImages = function (cssFileText) {
+    var _this = this;
+
+    (cssFileText.match(_this.imageUrlRegex) || []).forEach(function (url) {
+        var rewrittenUrl = "url('" + _this.rewriteUrl(url) + "')";
+        cssFileText = cssFileText.replace(url, rewrittenUrl);
+    });
+
+    return cssFileText;
+};
---
+++ src/jasmine-tests/bundle-image-rewrite-spec.js
@@ -0,0 +1,125 @@
+var exec = require('child_process').exec,
+    fs = require('fs'),
+    bundleImageRewrite = require('../bundle-image-rewrite.js');
+
+describe("BundleImageRewriter - ", function () {
+
+    var fileSystem, getImageRewriter, _exists, _filesToText, rootPath, outputRoot, _cssFileText;
+
+    beforeEach(function () {
+
+        outputRoot = "combined";
+        rootPath = "root/images/";
+        _cssFileText = null;
+
+        _filesToText = {};
+        _exists = false;
+        fileSystem = {};
+        fileSystem.existsSync = function() { return _exists; };
+        fileSystem.readFileSync = function (path) {
+            return _filesToText[path];
+        };
+
+        getImageRewriter = function() {
+            spyOn(fileSystem, 'existsSync').andCallThrough();
+            spyOn(fileSystem, 'readFileSync').andCallThrough();
+            var util = new bundleImageRewrite.BundleImageRewriter(fileSystem, outputRoot, rootPath);
+            return util;
+        };
+    });
+
+    describe("VersionImages: ", function () {
+
+        var fileContents1 = "AN IMAGE FILE 1", fileContents2 = "Something else that is an image too.";;
+
+        it("Given an image that doesnt exist, the image is not versioned.", function() {
+
+            var cssFileText = ".s { background: url('img/alert-03.jpg') center no-repeat; }";
+
+            givenImageDoesNotExist();
+            givenCssFileText(cssFileText);
+
+            versionImages();
+
+            verifyOutputTextIs(cssFileText);
+        });
+
+        it("Given an image that does exist, it reads the image off disk.", function () {
+
+            givenImageFile('img/an-image.jpg', fileContents1);
+            givenCssFileText(".s { background: url('img/an-image.jpg') center no-repeat; }");
+
+            versionImages();
+
+            expect(fileSystem.readFileSync).toHaveBeenCalledWith(rootPath + "img/an-image.jpg");
+        });
+
+        it("Given an image that does exist, it is versioned.", function () {
+
+            givenImageFile('img/an-image.jpg', fileContents1);
+            givenCssFileText(".s { background: url('img/an-image.jpg') center no-repeat; }");
+
+            versionImages();
+
+            verifyOutputTextIs(".s { background: url('combined/version-18458d2016656fdb823ed3ef01b8f8da/img/an-image.jpg') center no-repeat; }");
+        });
+
+        it("Image urls with double quotes are parsed correctly.", function () {
+
+            givenImageFile('img/an-image.jpg', fileContents1);
+            givenCssFileText('.s { background: url("img/an-image.jpg") center no-repeat; }');
+
+            versionImages();
+
+            verifyOutputTextIs(".s { background: url('combined/version-18458d2016656fdb823ed3ef01b8f8da/img/an-image.jpg') center no-repeat; }");
+        });
+
+        it("Image urls with no quotes are parsed correctly.", function () {
+
+            givenImageFile('img/another-image.jpg', fileContents1);
+            givenCssFileText('.s { background: url(img/another-image.jpg) center no-repeat; }');
+
+            versionImages();
+
+            verifyOutputTextIs(".s { background: url('combined/version-18458d2016656fdb823ed3ef01b8f8da/img/another-image.jpg') center no-repeat; }");
+        });
+
+        it("Multiple images in a single file are all versioned.", function () {
+
+            givenImageFile('img/an-image.jpg', fileContents1);
+            givenImageFile('img/another-image.jpg', fileContents2);
+            givenCssFileText('.a { background: url(img/another-image.jpg) center no-repeat; }\n'
+                            + '.b { background: url("img/an-image.jpg") center no-repeat; }\n'
+                            + '.c { background: url("img/another-image.jpg") no-repeat; }\n');
+
+            versionImages();
+
+            verifyOutputTextIs(".a { background: url('combined/version-570e1018fd20af9e8e22c860a43d0ac3/img/another-image.jpg') center no-repeat; }\n"
+                            + ".b { background: url('combined/version-18458d2016656fdb823ed3ef01b8f8da/img/an-image.jpg') center no-repeat; }\n"
+                            + ".c { background: url('combined/version-570e1018fd20af9e8e22c860a43d0ac3/img/another-image.jpg') no-repeat; }\n");
+        });
+
+
+        var givenImageDoesNotExist = function () {
+            _exists = false;
+        };
+
+        var givenImageFile = function (fileName, fileContents) {
+            _exists = true;
+            _filesToText[rootPath + fileName] = fileContents;
+        };
+
+        var givenCssFileText = function (text) {
+            _cssFileText = text;
+        };
+     
+        var versionImages = function () {
+            _outputFileText = getImageRewriter().VersionImages(_cssFileText);
+        };
+
+        var verifyOutputTextIs = function (expectedFile) {
+            expect(_outputFileText).toBe(expectedFile);
+        }
+
+    });
+});
