4f597049537ccd833716eb64d5061ea2dec8340a
matt-piziak-zocdoc <matt.piziak@zocdoc.com>
2014-06-26 20:16:55 -0400

    whoops, soft tabs

--- src/bundler.js
+++ src/bundler.js
@@ -497,7 +497,7 @@ function getOrCreateJsMustache(options, mustacheText, mPath, jsPath, cb /*cb(js)
 
 	compileAsync(options, "compiling", function (mustacheText, mPath, cb) {
             var templateName = path.basename(mPath, path.extname(mPath)); 
-			if (options.usetemplatedirs){
+            if (options.usetemplatedirs){
                 var splitPath = mPath.replace(".mustache", "").split(path.sep);
                 var templateIndex = splitPath.indexOf("templates");
                 templateName = splitPath.slice(templateIndex + 1).join("-");
