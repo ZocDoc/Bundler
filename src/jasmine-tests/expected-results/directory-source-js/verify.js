41cece9732e413b1e3c314d5382f576355e53c04
Thomas Huston <thomas.huston@zocdoc.com>
2015-03-20 14:26:56 -0400

    Update test case to ensure jsx files get included.

--- src/jasmine-tests/expected-results/directory-source-js/verify.js
+++ src/jasmine-tests/expected-results/directory-source-js/verify.js
@@ -4,4 +4,5 @@
 ;window.JST=window.JST||{},JST.mustache3=new Hogan.Template({code:function(a,b,c){var d=this;return d.b(c=c||""),d.b("<div> "),d.b(d.v(d.f("o",a,b,0))),d.b(" </div>"),d.b("\n"),d.fl()},partials:{},subs:{}})
 ;window.JST=window.JST||{},JST.mustache1=new Hogan.Template({code:function(a,b,c){var d=this;return d.b(c=c||""),d.b("<div> "),d.b(d.v(d.f("i",a,b,0))),d.b(" </div>"),d.b("\n"),d.fl()},partials:{},subs:{}})
 ;window.JST=window.JST||{},JST.mustache2=new Hogan.Template({code:function(a,b,c){var d=this;return d.b(c=c||""),d.b("<div> "),d.b(d.v(d.f("p",a,b,0))),d.b(" </div>"),d.b("\n"),d.fl()},partials:{},subs:{}})
+;var Hello=React.createClass({displayName:"Hello",render:function(){return React.createElement("div",null,"Hello ",this.props.name)}})
 ;var file4="file4"
