1eb5ec082fe8cab39bf37a0dd84f460cb4007496
Thomas Huston <thomas.huston@zocdoc.com>
2015-09-14 17:55:01 -0400

    [FE-144] Use the less import collection type.

--- src/collection/index.js
+++ src/collection/index.js
@@ -1,5 +1,6 @@
 var ArrayCollection = require('./array-collection.js');
 var StringCollection = require('./string-collection.js');
+var LessImportCollection = require('./less-import-collection.js');
 
 module.exports = {
     createAbConfigs: function(abConfigs) {
@@ -12,7 +13,7 @@ module.exports = {
         return new StringCollection(hash);
     },
     createLessImports: function(lessImports) {
-        return new ArrayCollection(lessImports);
+        return new LessImportCollection(lessImports);
     },
     createLocalizedStrings: function(localizedStrings) {
         return new ArrayCollection(localizedStrings);
