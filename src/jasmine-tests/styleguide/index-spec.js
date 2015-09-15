ecab4b0a45e84b0af17eb4c2ae66fe56e31cac75
Thomas Huston <thomas.huston@zocdoc.com>
2015-09-15 13:25:30 -0400

    [FE-144] Add util for detecting styleguide files.

---
+++ src/jasmine-tests/styleguide/index-spec.js
@@ -0,0 +1,88 @@
+var styleguide = require('../../styleguide');
+
+describe('Styleguide', function() {
+
+    describe('isStyleguideFile', function() {
+
+        var filePath,
+            isSgFile;
+
+        it('Given file directly in styleguide folder, returns true.', function() {
+
+            givenFilePathIs('C:/foo/styleguide/bar.less');
+
+            isStyleguideFile();
+
+            assertTrueWasReturned();
+
+        });
+
+        it('Given file nested in styleguide folder, returns true.', function() {
+
+            givenFilePathIs('C:/foo/styleguide/bar/bar.less');
+
+            isStyleguideFile();
+
+            assertTrueWasReturned();
+
+        });
+
+        it('Given file name contains styleguide, returns true.', function() {
+
+            givenFilePathIs('C:/foo/bar/bar.styleguide.less');
+
+            isStyleguideFile();
+
+            assertTrueWasReturned();
+
+        });
+        
+        it('Given file name starts with sg., returns true.', function() {
+        
+            givenFilePathIs('C:/foo/bar/sg.bar.less');
+
+            isStyleguideFile();
+
+            assertTrueWasReturned();
+        
+        });
+
+        it('Given file nested in folder starting with sg., returns false.', function() {
+
+            givenFilePathIs('C:/foo/sg.bar/bar.less');
+
+            isStyleguideFile();
+
+            assertFalseWasReturned();
+
+        });
+        
+        it('Given file not in styleguide folder, returns false.', function() {
+        
+            givenFilePathIs('C:/foo/bar/bar.less');
+
+            isStyleguideFile();
+
+            assertFalseWasReturned();
+        
+        });
+
+        var isStyleguideFile = function() {
+            isSgFile = styleguide.isStyleguideFile(filePath);
+        };
+
+        var givenFilePathIs = function(path) {
+            filePath = path;
+        };
+
+        var assertFalseWasReturned = function() {
+            expect(isSgFile).toBeFalsy();
+        };
+
+        var assertTrueWasReturned = function() {
+            expect(isSgFile).toBeTruthy();
+        };
+
+    });
+
+});
