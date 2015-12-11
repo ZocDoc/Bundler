d7f53050fc8cd7b41f523e7eff5807953488408a
Thomas Huston <thomas.huston@zocdoc.com>
2015-12-11 15:37:46 -0500

    Add in sprity stuff.

---
+++ src/generate-sprites.js
@@ -0,0 +1,31 @@
+var sprity = require('sprity');
+var minimist = require('minimist');
+var path = require('path');
+
+var options = minimist(process.argv.splice(2));
+
+var srcPath = path.join(options.imagesPath, '**', '*.png');
+
+sprity.create({
+    src: srcPath,
+    split: true,
+    spriteOutputPath: options.spritePath,
+    cssOutputPath: options.lessPath,
+    spriteUrlBase: options.spriteUrlBase,
+    name: 'sprite',
+    style: 'sprites.mixin.generated.less',
+    orientation: 'binary-tree',
+    prefix: 'sgv-icon',
+    'style-indent-size': 4,
+    processor: 'less',
+    template: 'template.hbs',
+    'dimension': [{
+        ratio: 1, dpi: 72
+    }, {
+        ratio: 2, dpi: 192
+    }]
+}, function(err) {
+    if (err) {
+        console.error(err);
+    }
+});
