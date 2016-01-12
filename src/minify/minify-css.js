684e2a744d8f85b9aa9384c9faf5120ca78faa27
Thomas Huston <thomas.huston@zocdoc.com>
2016-01-12 14:13:06 -0500

    Include source maps with minified CSS.

--- src/minify/minify-css.js
+++ src/minify/minify-css.js
@@ -1,10 +1,14 @@
 var CleanCss = require('clean-css');
+var path = require('path');
 var Promise = require('bluebird');
 
 /**
  * @param {object} options
  * @param {string} options.code
+ * @param {object} options.map
  * @param {string} options.inputPath
+ * @param {boolean} options.sourceMap
+ * @param {boolean} options.siteRoot
  * @returns {bluebird}
  */
 function minify(options) {
@@ -15,16 +19,24 @@ function minify(options) {
 
             var cleaner = new CleanCss({
                 advanced: false,
-                restructuring: false
+                restructuring: false,
+                sourceMap: options.sourceMap
             });
 
-            cleaner.minify(options.code, function (err, result) {
+            var css = {};
+            css[getFilePath(options.inputPath, options.map)] = {
+                styles: options.code,
+                sourceMap: JSON.stringify(options.map)
+            };
+
+            cleaner.minify(css, function (err, result) {
 
                 if (err) {
                     reject(err);
                 } else {
                     resolve({
-                        code: result.styles
+                        code: result.styles,
+                        map: result.sourceMap ? JSON.parse(result.sourceMap) : undefined
                     });
                 }
 
@@ -37,6 +49,16 @@ function minify(options) {
         }
 
     });
+
+}
+
+function getFilePath(inputPath, map) {
+
+    if (map) {
+        return path.basename(inputPath);
+    } else {
+        return inputPath;
+    }
 
 }
 
