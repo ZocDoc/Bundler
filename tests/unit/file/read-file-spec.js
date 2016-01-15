43fc3c8f6c8f644fa75c42615ec03801773262c5
Thomas Huston <thomas.huston@zocdoc.com>
2016-01-15 17:54:39 -0500

    Add tests for read file.

---
+++ tests/unit/file/read-file-spec.js
@@ -0,0 +1,81 @@
+var rewire = require('rewire');
+var read = rewire('../../../src/file/read-file');
+
+describe('read file', function() {
+
+    var text,
+        extractedCode;
+
+    beforeEach(function() {
+
+        spyOnReadTextFile();
+        spyOnSourceMapExtract();
+
+    });
+
+    var spyOnReadTextFile = function() {
+
+        read.__set__('readTextFile', function(filePath, cb) {
+            cb(text);
+        });
+
+    };
+
+    var spyOnSourceMapExtract = function() {
+
+        read.__set__('sourceMap', {
+            extract: function(text) {
+                return extractedCode;
+            }
+        });
+
+    };
+    
+    it('Returns code from file with source map extracted.', function(done) {
+
+        givenFileContentsIs('var x = 1;\n//# sourceMappingURL=data:application/json;base64,3459g490e5g');
+
+        givenExtractedCodeIs({
+            code: 'var x = 1;',
+            sourceMap: {
+                version: 3,
+                sources: ['foo.js']
+            }
+        });
+
+        readFile('C:\\foo.js')
+            .then(function(result) {
+
+                expect(result).toEqual({
+                    code: 'var x = 1;',
+                    sourceMap: {
+                        version: 3,
+                        sources: ['foo.js']
+                    }
+                });
+
+                done();
+
+            });
+    
+    });
+
+    var readFile = function(filePath) {
+
+        return read(filePath);
+
+    };
+
+    var givenFileContentsIs = function(contents) {
+
+        text = contents;
+
+    };
+
+    var givenExtractedCodeIs = function(code) {
+
+        extractedCode = code;
+
+    };
+
+});
