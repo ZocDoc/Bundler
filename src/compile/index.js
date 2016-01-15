4c4cc7b16bd6be03469a4ef439059313cb3c5de1
Thomas Huston <thomas.huston@zocdoc.com>
2016-01-15 13:27:35 -0500

    Specify the file types.

--- src/compile/index.js
+++ src/compile/index.js
@@ -4,11 +4,12 @@ var compileLESS = require('./compile-less');
 var compileMustache = require('./compile-mustache');
 var compileSASS = require('./compile-sass');
 var processCode = require('../process-code');
+var file = require('../file');
 
 module.exports = {
-    es6: processCode.with(compileES6),
-    jsx: processCode.with(compileJSX),
-    less: processCode.with(compileLESS),
-    mustache: processCode.with(compileMustache),
-    sass: processCode.with(compileSASS)
+    es6: processCode.with(file.type.JS, compileES6),
+    jsx: processCode.with(file.type.JS, compileJSX),
+    less: processCode.with(file.type.CSS, compileLESS),
+    mustache: processCode.with(file.type.JS, compileMustache),
+    sass: processCode.with(file.type.CSS, compileSASS)
 };
