46bd3ca1550479ac4e654025a3e2183f620b154f
Thomas Huston <thomas.huston@zocdoc.com>
2016-01-22 13:37:51 -0500

    Add no-op compile tasks for JS and CSS.

--- src/compile/index.js
+++ src/compile/index.js
@@ -1,11 +1,15 @@
+var compileCSS = require('./compile-css');
 var compileES6 = require('./compile-es6');
+var compileJS = require('./compile-js');
 var compileJSX = require('./compile-jsx');
 var compileLESS = require('./compile-less');
 var compileMustache = require('./compile-mustache');
 var compileSASS = require('./compile-sass');
 
 module.exports = {
+    css: compileCSS,
     es6: compileES6,
+    js: compileJS,
     jsx: compileJSX,
     less: compileLESS,
     mustache: compileMustache,
