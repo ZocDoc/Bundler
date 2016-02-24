3c9051cf8a965d7a764402c5602f2d663c54518a
Thomas Huston <thomas.huston@zocdoc.com>
2016-02-24 17:35:04 -0500

    Format this differently so it'll work properly.

--- src/compile/compile-es6.js
+++ src/compile/compile-es6.js
@@ -1,5 +1,7 @@
 var babel = require('babel-core');
 
+var LOOSE = { loose: true };
+
 /**
  * @param {object} options
  * @param {string} options.code
@@ -13,12 +15,30 @@ function compile(options) {
 
         var settings = {
             plugins: [
+                [require('babel-plugin-transform-es2015-classes'), LOOSE],
+                [require('babel-plugin-transform-es2015-computed-properties'), LOOSE],
+                [require('babel-plugin-transform-es2015-destructuring'), LOOSE],
+                [require('babel-plugin-transform-es2015-for-of'), LOOSE],
+                [require('babel-plugin-transform-es2015-modules-commonjs'), LOOSE],
+                [require('babel-plugin-transform-es2015-spread'), LOOSE],
+                [require('babel-plugin-transform-es2015-template-literals'), LOOSE],
+                require('babel-plugin-check-es2015-constants'),
                 require('babel-plugin-syntax-async-functions'),
+                require('babel-plugin-transform-es2015-arrow-functions'),
+                require('babel-plugin-transform-es2015-block-scoped-functions'),
+                require('babel-plugin-transform-es2015-block-scoping'),
+                require('babel-plugin-transform-es2015-function-name'),
+                require('babel-plugin-transform-es2015-literals'),
+                require('babel-plugin-transform-es2015-object-super'),
+                require('babel-plugin-transform-es2015-parameters'),
+                require('babel-plugin-transform-es2015-shorthand-properties'),
+                require('babel-plugin-transform-es2015-sticky-regex'),
+                require('babel-plugin-transform-es2015-typeof-symbol'),
+                require('babel-plugin-transform-es2015-unicode-regex'),
                 require('babel-plugin-transform-proto-to-assign'),
                 require('babel-plugin-transform-regenerator')
             ],
             presets: [
-                require('babel-preset-es2015-loose'),
                 require('babel-preset-react')
             ]
         };
